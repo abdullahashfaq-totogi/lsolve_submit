79d51df1568356b3ebe4dc281803f37213645ab9
abdullahashfaq-totogi <abdullah.ashfaq@totogi.com>
2023-02-09 03:29:57 +0500

    Added sample data

--- config.h
+++ config.h
@@ -11,11 +11,11 @@ typedef double matrix_dtype;
 //#define L_matrix_path "../data/ab_m1.mtx";
 //#define b_matrix_path "../data/b_for_ab_m1_sparse.mtx";
 
-#define L_matrix_path "../data/torso1.mtx";
-#define b_matrix_path "../data/b_for_torso1.mtx";
+//#define L_matrix_path "../data/torso1.mtx";
+//#define b_matrix_path "../data/b_for_torso1.mtx";
 
-//#define L_matrix_path "../data/TSOPF_RS_b678_c2.mtx";
-//#define b_matrix_path "../data/b_for_TSOPF_RS_b678_c2_b.mtx";
+#define L_matrix_path "../data/TSOPF_RS_b678_c2.mtx";
+#define b_matrix_path "../data/b_for_TSOPF_RS_b678_c2_b.mtx";
 
 
 #endif //LSOLVE_SUBMIT_CONFIG_H
